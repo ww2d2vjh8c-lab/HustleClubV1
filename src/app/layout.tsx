3ccbf374f3564e9b010152ab9cc42e3532cddb13
Ayush Kaushik <coc123.1607@gmail.com>
2026-04-17 06:01:02 +0530

    fix(design): remove body isolate (Safari fixed bug), document hero z-index constraint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,7 +65,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className={`${bebasNeue.variable} ${spaceGrotesk.variable} ${spaceMono.variable}`}>
-      <body className="font-[var(--font-body)] antialiased isolate">
+      <body className="font-[var(--font-body)] antialiased">
         <Navbar user={user} role={role} avatarUrl={avatarUrl} initials={initials} />
         <div className="pb-16">{children}</div>
       </body>
